561b0551f945133990015c86e8c6260dd0806022
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-09-26 11:07:35 +0300

    Creating and updating array in js file.js
    Creating and updating array in js file.

---
+++ codeWallet/Creating and updating array in js file.js
@@ -0,0 +1,11 @@
+var myArray = ["Number", 100, "False"]; //Literal array storing different data types
+var array3 = []; //initializing array with no default value.
+
+var myArray2 = new Array(20, "myself", "I", "you"); //object array. This is the most used array initialization.
+var array4 = new Array(); //initializing array with no default value.
+console.log(myArray);
+myArray[2];
+
+//Updating an existing array.
+myArray[0] = 50;
+console.log(myArray);
