f1511fb1b54d22c2e45c9c829831014b1cbfd17e
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-09-30 06:45:39 +0300

    Creating object.js
    Creating object in JavaScript.

---
+++ codeWallet/Creating object.js
@@ -0,0 +1,9 @@
+var arr = new Array(1, 2, 3, 4, 5, 6);
+var obj = {color:"blue", make:"ford", model:5}
+console.log(obj);
+
+var obj1 = new Object();
+obj1.color = "red";
+obj1.model = 8;
+obj1.style = "none";
+console.log(obj1);
