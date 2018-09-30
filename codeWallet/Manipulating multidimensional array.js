4eec24ce0cfaa0d7bc69b805d92ea1d0bcf5248d
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-09-30 05:26:30 +0300

    Manipulating multidimensional array.js
    Manipulating multidimensional array in JavaScript.

---
+++ codeWallet/Manipulating multidimensional array.js
@@ -0,0 +1,8 @@
+var animals = new Array([3, 78, 1, 50, 20, 20], ["dog", "cat", "pig"]);
+
+var i = prompt("Select row");
+var j = prompt("Select column");
+
+var result = animals[i][j];
+
+console.log("You selected \"" + result + "\"");
