6b6dd9d984b102e95969a62346f3234ac794a110
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-01 05:04:51 +0300

    Delete Guessing game in js Array.js

--- codeWallet/Guessing game in js Array.js
+++
@@ -1,7 +0,0 @@
-var animal = new Array("dog", "sheep", "goat", "camel", "cat", "rabbit", "monkey");
-var randomIndex = Math.floor(Math.random()*animal.length);
-var randomAnimal = animal[randomIndex];
-
-var message = prompt("Which animal value is "+ randomAnimal + "?");
-var response = (message == randomIndex)? "Correct!" : "Wrong, it is "+ randomIndex;
-alert(response);
