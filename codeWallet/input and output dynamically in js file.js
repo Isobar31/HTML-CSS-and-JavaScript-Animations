f169e838da71b62aaef80cc1716f411900d03978
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-01 05:10:20 +0300

    Delete input and output dynamically in js file.js

--- codeWallet/input and output dynamically in js file.js
+++
@@ -1,5 +0,0 @@
-var task = ["do assignment", "take a sport", "cook", "read", "meet with friends"];
-alert(" \"1 = do assignment.\" \"2 = take a sport.\" \"3 = cook.\" \"4 = read.\" \"5 = meet with friends\"");
-var question = prompt("What would you like to do now from the above list?");
-
-alert("Thank you for answering. So you would like to " + task[(question - 1)]);
