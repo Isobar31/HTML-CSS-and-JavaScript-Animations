cdc7cad4a11f01d6dd1b8952f576b443f9aa7196
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-01 05:13:34 +0300

    Delete Manipulating objects in the console.js

--- codeWallet/Manipulating objects in the console.js
+++
@@ -1,7 +0,0 @@
-var car = {color: "green", make: "Sanyang", model: 501};
-car.color = "blue";
-console.log(car);
-
-// we can manipulate the following in the console of the browser in order to make changes in the object.
-car["color"] = "black"; // changes the color.
-delete car.color // deletes the color property of the object.
