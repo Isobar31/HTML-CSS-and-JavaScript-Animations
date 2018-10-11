a9010f346bf4784f2c25aeead8093819da9e6758
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-11 03:43:25 +0300

    presenting the object inside object in array form.js
    Presenting the object inside object in array form.

---
+++ codeWallet/Objects in JavaScript/presenting the object inside object in array form.js
@@ -0,0 +1,18 @@
+var Isaac = {
+  name:"Isaac Thomas"
+  , age:25
+  , appearance:"thick bodied"
+};
+
+
+var friends = {
+  friend1:Isaac
+  , friend2:{
+    name:"Tari-ebi Suobo"
+    , age:26
+    , appearance:"slim"
+  }
+};
+
+var friend1 = [Isaac, {name:"Tari-ebi Suobo", age:26, appearance:"slim"}];
+// console.log();
