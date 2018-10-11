71a07b1c4a64b641ce18ae60f2579058f412589f
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-11 03:32:47 +0300

    Creating objects inside object.js
    Creating objects inside object.

---
+++ codeWallet/Objects in JavaScript/Creating objects inside object.js
@@ -0,0 +1,14 @@
+var Isaac = {
+  name:"Isaac Thomas"
+  , age:25
+  , appearance:"thick bodied"
+};
+
+var Tari = {
+  name:"Tari-ebi Suobo"
+  , age:26
+  , appearance:"slim"
+};
+
+var friends = {friend1:Isaac, friend2:Tari};
+// console.log();
