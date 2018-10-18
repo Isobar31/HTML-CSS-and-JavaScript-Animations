4a99837f58658030a536e1c8b4bec0be0dc011e2
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-18 04:57:55 +0300

    static method.cs

---
+++ codeWallet/CSharp/Adding values into histogram/static method.cs
@@ -0,0 +1,7 @@
+            chartHistogram.Series["Salary"].Points.AddXY("Theo", 12000);
+            chartHistogram.Series["Salary"].Points.AddXY("Audrey", 4500);
+            chartHistogram.Series["Salary"].Points.AddXY("Francis", 3000);
+            chartHistogram.Series["Salary"].Points.AddXY("Isaac", 4000);
+            chartHistogram.Series["Salary"].Points.AddXY("Annie", 14000);
+            chartHistogram.Series["Salary"].Points.AddXY("Fatou", 7500);
+            chartHistogram.Series["Salary"].Points.AddXY("Brender", 7000);
