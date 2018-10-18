2b04f7ff9f974889bada359b0e3f898ff653da98
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2018-10-18 04:59:46 +0300

    static method 2.cs

---
+++ codeWallet/CSharp/Adding values into histogram/static method 2.cs
@@ -0,0 +1,36 @@
+chartHistogram.Series["Salary"].Points.Add(1000);
+            chartHistogram.Series["Salary"].Points[0].Color = Color.Red;
+            chartHistogram.Series["Salary"].Points[0].AxisLabel = "Peter";
+            chartHistogram.Series["Salary"].Points[0].LegendText = "Peter";
+            chartHistogram.Series["Salary"].Points[0].Label = "1000";
+            //
+            chartHistogram.Series["Salary"].Points.Add(5000);
+            chartHistogram.Series["Salary"].Points[1].Color = Color.Green;
+            chartHistogram.Series["Salary"].Points[1].AxisLabel = "John";
+            chartHistogram.Series["Salary"].Points[1].LegendText = "John";
+            chartHistogram.Series["Salary"].Points[1].Label = "5000";
+            //
+            chartHistogram.Series["Salary"].Points.Add(1500);
+            chartHistogram.Series["Salary"].Points[2].Color = Color.White;
+            chartHistogram.Series["Salary"].Points[2].AxisLabel = "Tari";
+            chartHistogram.Series["Salary"].Points[2].LegendText = "Tari";
+            chartHistogram.Series["Salary"].Points[2].Label = "1500";
+            //
+            chartHistogram.Series["Salary"].Points.Add(2000);
+            chartHistogram.Series["Salary"].Points[3].Color = Color.Plum;
+            chartHistogram.Series["Salary"].Points[3].AxisLabel = "Victor";
+            chartHistogram.Series["Salary"].Points[3].LegendText = "Victor";
+            chartHistogram.Series["Salary"].Points[3].Label = "2000";
+            //
+            chartHistogram.Series["Salary"].Points.Add(6000);
+            chartHistogram.Series["Salary"].Points[4].Color = Color.Orange;
+            chartHistogram.Series["Salary"].Points[4].AxisLabel = "Miranda";
+            chartHistogram.Series["Salary"].Points[4].LegendText = "Miranda";
+            chartHistogram.Series["Salary"].Points[4].Label = "6000";
+            //
+            chartHistogram.Series["Salary"].Points.Add(4500);
+            chartHistogram.Series["Salary"].Points[5].Color = Color.OliveDrab;
+            chartHistogram.Series["Salary"].Points[5].AxisLabel = "Theo";
+            chartHistogram.Series["Salary"].Points[5].LegendText = "Theo";
+            chartHistogram.Series["Salary"].Points[5].Label = "4500";
+            
