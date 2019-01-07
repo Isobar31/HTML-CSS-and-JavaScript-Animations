1d3eaccffa960eba1f0485da7c06fe2d5573d097
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-01-07 03:26:15 +0200

    Date methods.js

---
+++ codeWallet/JavaScript/Date methods.js
@@ -0,0 +1,10 @@
+var date = new Date();
+document.write(date + "<br>");
+
+var getYear = date.getFullYear();
+document.write(getYear + "<br>");
+
+var now = date.getMonth(); /*You can check the JS Date built-in methods 
+                            for date in JavaScript Date MSDN online 
+                            and play with them all. */
+document.write(now + "<br>");
