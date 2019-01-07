395724595a6421403b2921920c812658a9cdfad1
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-01-07 03:54:25 +0200

    Object.js

---
+++ codeWallet/JavaScript/Object.js
@@ -0,0 +1,11 @@
+// object
+var name;
+var age;
+var country;
+
+var person = new Object();
+person.name = "Isoboye";
+person.age = 32;
+person.country = "Nigeria";
+
+document.write(person.name + "<br>" + person.age + "<br>" + person.country);
