fd0e5c0e8c62ad4627899373c7e3f464def47d1f
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-01-07 03:09:12 +0200

    Positions of letters in a string.js

---
+++ codeWallet/JavaScript/Positions of letters in a string.js
@@ -0,0 +1,8 @@
+var a = "I am Vincent";
+var total = a.length;
+var b = a.charAt(5);// Method for checking string specified values.
+var c = a.search("c"); // The opposite of the method charAt().
+document.write(total + "<br/>");// Shows the total length of the sentence.
+
+document.write(b + "<br>");
+document.write(c);
