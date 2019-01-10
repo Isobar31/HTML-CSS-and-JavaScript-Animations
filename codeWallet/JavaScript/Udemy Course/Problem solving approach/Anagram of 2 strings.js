2acea21baa558f987b22edf30dea5dfeb6b1e836
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-01-11 01:53:35 +0200

    Anagram of 2 strings.js

--- codeWallet/JavaScript/Udemy Course/Problem solving approach/Anagram of 2 strings.js
+++ codeWallet/JavaScript/Udemy Course/Problem solving approach/Anagram of 2 strings.js
@@ -1,2 +1,35 @@
 //Problem: Given two strings, write a function to determine if the 2nd string is an anagram of the 1st (e.g cinema == iceman). 
-in progress...
+
+function AnagramStrings(str1, str2)
+{   
+    if(str1 !== str2)
+    {        
+       return false;
+    }
+     
+  const loopUp = {};
+
+for(var i = 0; i < str1.length; i++)
+{
+    var letter = str1[i];
+    loopUp[letter] ? loopUp[letter] += 1 : loopUp[letter] = 1;
+}
+console.log(loopUp);
+
+for(var i = 0; i < str2.length; i++)
+{
+    var letter = str2[i];
+    if(!loopUp[letter])
+    {
+        return false;
+    }
+    else
+    {
+        loopUp[letter] -= 1;
+    }
+}
+ return true;
+}
+
+AnagramStrings("Welcome", "Welcome");
+
