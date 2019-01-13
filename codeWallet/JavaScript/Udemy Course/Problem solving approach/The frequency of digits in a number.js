c073f6db29cbc2851949479a04df6021169483ab
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-01-14 00:14:44 +0200

    The frequency of digits in a number.js

--- codeWallet/JavaScript/Udemy Course/Problem solving approach/The frequency of digits in a number.js
+++ codeWallet/JavaScript/Udemy Course/Problem solving approach/The frequency of digits in a number.js
@@ -61,3 +61,32 @@ function sameFrequency(first, second) {
     }
     return true;
 }
+
+
+
+// The 3rd approach
+function sameFrequency(first, second) {
+ 
+    // get the number of their digits
+    const firstLen = Math.floor(Math.log10(first)) + 1;
+    const secondLen = Math.floor(Math.log10(second)) + 1;
+     
+    if (firstLen !== secondLen) {
+        return false;
+    }
+     
+    let freqCounter = {};
+    for (let digitPos = 0; digitPos < firstLen; ++digitPos) {
+        let digit = Math.floor(first / Math.pow(10, digitPos)) % 10;
+        freqCounter[digit] = freqCounter[digit] + 1 || 1;
+    }
+    for (let digitPos = 0; digitPos < secondLen; ++digitPos) {
+        let digit = Math.floor(second / Math.pow(10, digitPos)) % 10;
+        if (!freqCounter[digit]) {
+            return false;
+        }
+        freqCounter[digit]--;
+    }
+    return true;
+     
+    }
